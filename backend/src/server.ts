e45dc79b1bde85d82cf849f6238c4e92fb1b2fa7
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-06-02 11:46:35 -0300

    migrations and seeed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { json } from 'express';
+import path from 'path';
 
 import routes from './routes';
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(routes);
 app.use(json());
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => {
   console.log('🚀 server started on port 3333');
